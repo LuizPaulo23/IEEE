e143c12769afa229e28b575936dcd72a63e033f5
LuizPaulo23 <luizpauloueg@gmail.com>
2024-08-01 19:24:58 -0300

    CHANGED: análise e pipeline em linguagem R finalizado

--- primeiro_periodo/analyze_first_period.R
+++ primeiro_periodo/analyze_first_period.R
@@ -11,8 +11,9 @@ setwd("~/Github/Projetos/IEEE/primeiro_periodo/archive")
 
 # Dependências =================================================================
 
-pacman::p_load(tidyverse, caret, 
-               data.table, Metrics,DataExplorer, rsample, glmnet, stats)
+pacman::p_load(tidyverse, data.table, 
+               DataExplorer, naniar, 
+               Metrics, vip, dlookr, caret, rsample, glmnet, stats)
 
 # FUNÇÕES ======================================================================
 
@@ -58,8 +59,10 @@ get_clean <- function(){
   # Seleção manual  
   
       data_clean = data_for_clean %>% 
-                   dplyr::mutate(month = str_sub(last_scraped, start = 6, end = 7)) %>% 
-                          select(month, 
+                   dplyr::mutate(month = str_sub(last_scraped, start = 6, end = 7), 
+                                 year = str_sub(last_scraped, start = 1, end = 4)) %>% 
+                          select(month,
+                                 year,
                                  host_is_superhost, 
                                  latitude:number_of_reviews, 
                                  property_type, 
@@ -68,6 +71,7 @@ get_clean <- function(){
                           filter(host_is_superhost != 3) %>%
                           # como numéricos e factor 
                           mutate(month = as.factor(month),
+                                 year = as.factor(year), 
                                  host_is_superhost = as.factor(host_is_superhost),
                                  accommodates = as.numeric(accommodates), 
                                  minimum_nights = as.numeric(minimum_nights),
@@ -86,6 +90,19 @@ get_clean <- function(){
   return(data_clean)
 }
 
+
+# get_transformation: transformação dos dados, feature engine 
+
+get_transformation <- function(transformation){
+
+data_clean_transf = data_clean %>% 
+                    dplyr::mutate_if(is.numeric, 
+                                     ~dlookr::transform(., method = transformation))
+
+
+return(data_clean_transf)
+
+}
 
 # run_model_select: Modelo LASSO para seleção de variáveis 
 
@@ -117,9 +134,31 @@ run_model_select <- function(db_clean, dist){
   
 }
 
+# get_metrics: retorna as mátricas de acurácia =================================
+
+get_metrics <- function(actual, predicted, model_name) {
+  
+  cat("\nMetrics for", model_name, "model:\n")
+  mae_value = Metrics::mae(actual, predicted)
+  cat("MAE:", mae_value, "\n")
+  
+  rmse_value = Metrics::rmse(actual, predicted)
+  cat("RMSE:", rmse_value, "\n")
+  
+  mse_value = Metrics::mse(actual, predicted)
+  cat("MSE:", mse_value, "\n")
+  
+  mape_value = Metrics::mape(actual, predicted)
+  cat("MAPE:", mape_value, "\n")
+  
+}
+
 # Chamando as FUNÇÕES ==========================================================
 
-data_clean = get_clean()
+data_clean_train = get_clean()
+
+# getwd()
+# writexl::write_xlsx(data_clean_train, "data_clean_train.xlsx")
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # ISSO PODE DEMORAR!!!!
@@ -127,7 +166,7 @@ data_clean = get_clean()
 # DESNECESSÁRIO NA PREDIÇÃO COM PIPELINE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-# model_lasso = run_model_select(db_clean = data_clean, dist = "gaussian")
+model_lasso = run_model_select(db_clean = data_clean_train, dist = "gaussian")
 
 # Import Dataset & EDA =========================================================
 
@@ -142,7 +181,7 @@ DataExplorer::plot_correlation(numeric_vars)
 
 # Resultados do LASSO: 
 
-lasso_coefficients <- coef(model_lasso)
+lasso_coefficients <- coef(model_lasso) %>% print()
 
 vip::vip(model_lasso, 
          lambda = model_lasso[5],
@@ -153,15 +192,65 @@ vip::vip(model_lasso,
 
 # MODELAGEM \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-pipeline <- function(pct_train, db){
+# db = get_transformation(transformation)
 
-data_split = rsample::initial_split(data = db, prop = pct_train)
+data_split = rsample::initial_split(data = data_clean_train, prop = 0.75)
 train = rsample::training(data_split)
 test  = rsample::testing(data_split)
 
 # Baseline: Modelo de regressão múltiplo =======================================
 
 model_lm <- stats::lm(formula = price ~., data = train)
+
+summary(model_lm)
+
+stats::ks.test(model_lm$residuals,
+               "pnorm",
+               mean = mean(model_lm$residuals),
+               sd = sd(model_lm$residuals))
+
+colinearidade <- train %>% 
+                 dplyr::mutate(residuals = model_lm$residuals) %>% 
+                 dplyr::select(-month, - year) 
+
+cor(colinearidade)
+DataExplorer::plot_correlation(colinearidade)
+
+# OTIMIZANDO O BASELINE ========================================================
+# RETIRAR VARIÁVEIS NÃO SIGNIFICATIVAS E COM MULICOLINEARIDADE 
+
+# vip::vip(model_lm)
+
+# vip::vi_model(model_lasso)
+# Usando o LASSO COMO PARÂMETRO DE SELEÇÃO DE VARIÁVEIS 
+
+# data_clean_train <- data_clean_train %>% 
+#                     dplyr::select(price, 
+#                                   latitude,
+#                                   longitude, 
+#                                   bedrooms,
+#                                   accommodates, 
+#                                   year, 
+#                                   number_of_reviews, 
+#                                   minimum_nights)
+
+# Modelo de regressão múltipica otimizado 
+
+
+# model_lm <- stats::lm(formula = price ~., data = data_clean_train)
+
+# summary(model_lm)
+
+# stats::ks.test(model_lm$residuals,
+#                "pnorm",
+#                mean = mean(model_lm$residuals),
+#                sd = sd(model_lm$residuals))
+# 
+# colinearidade <- data_clean_train %>% 
+#                  dplyr::mutate(residuals = model_lm$residuals) 
+# 
+# cor(colinearidade)
+# DataExplorer::plot_correlation(colinearidade)
 
 # Random Forests ===============================================================
 
@@ -196,62 +285,82 @@ model_rf_cv <- train(price ~ .,
 
 # Rede Neural ==================================================================
 
-control <- caret::trainControl(method = "cv", number = 10)
-
-tune_grid <- base::expand.grid(size = seq(1, 10, by = 1),     # Tamanhos de 1 a 10 neurônios na camada oculta
-                              decay = c(0, 0.001, 0.01, 0.1))
-
-model_nn_cv <- caret::train(price ~ ., 
-                            data = train, 
-                            method = "nnet", 
-                            trControl = control, 
-                            tuneGrid = tune_grid, 
-                            linout = TRUE, 
-                            maxit = 100,     # Número máximo de iterações
-                            trace = FALSE)   # Desativar saída de treino
+# control <- caret::trainControl(method = "cv", number = 1)
+# 
+# tune_grid <- base::expand.grid(size = seq(1, 10, by = 1),     # Tamanhos de 1 a 10 neurônios na camada oculta
+#                               decay = c(0, 0.001, 0.01, 0.1))
+# 
+# model_nn_cv <- caret::train(price ~ ., 
+#                             data = train, 
+#                             method = "nnet", 
+#                             trControl = control, 
+#                             tuneGrid = tune_grid, 
+#                             linout = TRUE, 
+#                             maxit = 1,     # Número máximo de iterações
+#                             trace = FALSE)   # Desativar saída de treino
 
 
 # Predições: 
 
 results = test %>%
-          dplyr::mutate(lm = stats::predict(model_lm, newdata = test), 
+          dplyr::mutate( 
                         random_forest = stats::predict(model_rf, data = test)$predictions,
                         random_forest_cv = stats::predict(model_rf_cv, newdata = test),
-                        model_nn_cv = stats::predict(model_nn_cv, newdata = test),
-                        combined = (lm + random_forest + random_forest_cv + model_nn_cv)/4) %>% 
-          dplyr::select(price, lm, random_forest, random_forest_cv, model_nn_cv, combined)
+                        # model_nn_cv = stats::predict(model_nn_cv, newdata = test),
+                        combined = (random_forest + random_forest_cv)/2) %>% 
+          dplyr::select(price, random_forest, random_forest_cv, combined)
 
 # métricas ====================================================================
 
-calculate_metrics <- function(actual, predicted, model_name) {
-  
-  cat("\nMetrics for", model_name, "model:\n")
-  mae_value = mae(actual, predicted)
-  cat("MAE:", mae_value, "\n")
-  
-  rmse_value = rmse(actual, predicted)
-  cat("RMSE:", rmse_value, "\n")
-  
-  mse_value = mse(actual, predicted)
-  cat("MSE:", mse_value, "\n")
-  
-  mape_value = mape(actual, predicted)
-  cat("MAPE:", mape_value, "\n")
-  
-}
-
 # Métricas para cada modelo
 
-calculate_metrics(results$price, results$lm, "Linear Regression")
-calculate_metrics(results$price, results$random_forest, "Random Forest")
-calculate_metrics(results$price, results$random_forest_cv, "Random Forest with CV")
-calculate_metrics(results$price, results$model_nn_cv, "Neural Network with CV")
-calculate_metrics(results$price, results$combined, "Combined")
+# get_metrics(results$price, results$lm, "Linear Regression")
+get_metrics(results$price, results$random_forest, "Random Forest")
+get_metrics(results$price, results$random_forest_cv, "Random Forest with CV")
+# get_metrics(results$price, results$model_nn_cv, "Neural Network with CV")
+get_metrics(results$price, results$combined, "Combined")
 
-return(results)
+mean(train$price)
+mean(test$price)
+mean(results$random_forest)
+mean(results$random_forest_cv)
+mean(results$combined)
 
-}
+# TESTE: PREDIÇÃO FORA DA AMOSTRA DE TREINO ======================================
+
+data_test = get_import(db = "test")
+
+DataExplorer::plot_intro(data = data_test)
+naniar::gg_miss_var(data_test)
+
+data_clean_test = data_test %>% 
+                  dplyr::mutate(month = str_sub(last_scraped, start = 6, end = 7), 
+                                year = str_sub(last_scraped, start = 1, end = 4)) %>% 
+                  select(month,
+                         year,
+                         latitude:number_of_reviews) %>% 
+                  mutate(month = as.factor(month),
+                         year = as.factor(year), 
+                         accommodates = as.numeric(accommodates), 
+                         minimum_nights = as.numeric(minimum_nights),
+                         extra_people = as.numeric(gsub("[\\$,]", "", extra_people)), 
+                         beds = ifelse(is.na(beds), median(beds, na.rm = T), beds), 
+                         bathrooms = ifelse(is.na(bathrooms), median(bathrooms, na.rm = T), bathrooms), 
+                         bedrooms = ifelse(is.na(bedrooms), median(bedrooms, na.rm = T), bedrooms))
 
 
-result = pipeline(db = data_clean, pct_train = 0.75)
+naniar::gg_miss_var(data_clean_test)
+# writexl::write_xlsx(data_clean_test, "data_clean_test.xlsx")
 
+predict_random_forest = stats::predict(model_rf, data = data_clean_test)$predictions
+
+predict_random_forest <- predict_random_forest %>%  
+                         base::as.data.frame() %>% 
+                         dplyr::mutate(ID = data_clean_test$id) %>% 
+                                       relocate(ID, .after = NULL) %>% 
+                                       rename("price" = ".")
+
+writexl::write_xlsx(predict_random_forest, "predictions.xlsx")
+utils::write.csv(predict_random_forest, "predictions.csv", row.names = FALSE)
+
+## FIM
